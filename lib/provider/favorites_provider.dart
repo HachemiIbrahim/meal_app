7ed3b88e03119e4ba505c671eaa05f8cd59ec7b5
Hachemi Ibrahim <123593839+HachemiIbrahim@users.noreply.github.com>
2023-09-08 17:20:02 +0100

    favorite provider

---
+++ lib/provider/favorites_provider.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../model/meal.dart';
+
+class FavoriteMealNotifier extends StateNotifier<List<Meal>> {
+  FavoriteMealNotifier() : super([]);
+
+  void toogleFavoriteMeal(Meal meal) {
+    final isFavorites = state.contains(meal);
+
+    if (isFavorites) {
+      state = state.where((m) => m.id != meal).toList();
+    } else {
+      state = [...state, meal];
+    }
+  }
+}
