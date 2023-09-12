bd3dad49808fecde2f6d3596a7dd5110fa561eb0
Hachemi Ibrahim <123593839+HachemiIbrahim@users.noreply.github.com>
2023-09-12 21:18:37 +0100

    animating star button

--- lib/screen/meal_detail.dart
+++ lib/screen/meal_detail.dart
@@ -30,7 +30,16 @@ class MealDetail extends ConsumerWidget {
                 ),
               );
             },
-            icon: Icon(isFavorite ? Icons.star : Icons.star_border),
+            icon: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 300),
+              transitionBuilder: (child, animation) {
+                return RotationTransition(
+                  turns: animation,
+                  child: child,
+                );
+              },
+              child: Icon(isFavorite ? Icons.star : Icons.star_border),
+            ),
           ),
         ],
         title: Text(meal.title),
